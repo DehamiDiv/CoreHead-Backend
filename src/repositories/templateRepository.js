24b495321c7fddba77b0a7a1266c4b21f1b5ee8c
Pipu404 <piyasooriyapipuni@gmail.com>
2026-04-23 02:06:01 -0600

    refactor: remove inline comment from module exports in templateRepository

--- src/repositories/templateRepository.js
+++ src/repositories/templateRepository.js
@@ -131,7 +131,7 @@ module.exports = {
     updateTemplate,
     deleteTemplate,
     saveTemplateHistory,
-    // My contribution
+    
     publishTemplate,
     assignTemplate,
     resolveActiveLayout
